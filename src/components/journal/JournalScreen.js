b49fc09d69429e917fec3208acda6844c80143c1
Pablo Arce <arcepablo07@gmail.com>
2022-04-11 14:29:14 -0300

    Sidebar

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./Sidebar";
 
 const JournalScreen = () => {
   return (
-    <div><h1>JournalScreen</h1></div>
-  )
-}
+    <div className="journal__main-content">
+      <Sidebar/>
+      <main>
+        <h1>Main content</h1>
+      </main>
+    </div>
+  );
+};
 
-export default JournalScreen
+export default JournalScreen;
