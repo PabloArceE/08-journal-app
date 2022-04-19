f4cbe21d93ebb0ccc8c52a863112b8d8e5b0dfe9
Pablo Arce <arcepablo07@gmail.com>
2022-04-19 09:43:26 -0300

    estructura y diseño del HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import NotesAppBar from './NotesAppBar'
+import React from "react";
+import NotesAppBar from "./NotesAppBar";
 
 const NoteScreen = () => {
   return (
-    <div className='notes__main-content'>
-        <NotesAppBar />
+    <div className="notes__main-content">
+      <NotesAppBar />
+      <div className="notes__content">
+          <input
+            type='text'
+            placeholder="Some awesome title"
+            className="notes__title-input"
+            autoComplete="off"
+          />
+          <textarea
+            placeholder="What happened today"
+            className="notes__textarea"
+          ></textarea>
+          <div className="notes__image">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/3/35/Neckertal_20150527-6384.jpg"
+              alt="se rompió el enlace, lo siento" 
+            />
+          </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NoteScreen
+export default NoteScreen;
--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,19 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const NotesAppBar = () => {
   return (
-    <div className='notes__appbar'>
-        <span>Wednesday April 13, 2022</span>
-        <div>
-            <button className='btn'>
-                Picture
-            </button>
-            <button className='btn'>
-                Save
-            </button>
-        </div>
+    <div className="notes__appbar">
+      <span>Wednesday April 13, 2022</span>
+      <div>
+        <button className="btn">Picture</button>
+        <button className="btn">Save</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotesAppBar
+export default NotesAppBar;
